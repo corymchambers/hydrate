bf90215e2cad1dc32df7b00a874902a7a3dc7d84
Cory Chambers <corymchambers1@gmail.com>
2025-06-29 09:55:43 -0400

    Remove screen headers.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,9 @@ import { Tabs } from 'expo-router';
 export default function RootLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="history" options={{ title: 'History' }} />
-      <Tabs.Screen name="settings" options={{ title: 'Settings' }} />
+      <Tabs.Screen name="index" options={{ title: 'Home', headerShown: false }} />
+      <Tabs.Screen name="history" options={{ title: 'History', headerShown: false }} />
+      <Tabs.Screen name="settings" options={{ title: 'Settings', headerShown: false }} />
     </Tabs>
   );
 }
